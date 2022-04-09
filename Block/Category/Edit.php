600416ac197d72e067692fea2212730d5b7e94d8
kvlcode <kavalprajapati126@gmail.com>
2022-04-09 10:51:33 +0530

    mix

--- Block/Category/Edit.php
+++ Block/Category/Edit.php
@@ -6,41 +6,5 @@ class Block_Category_Edit extends Block_Core_Edit
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->setTemplate('view/category/edit.php');
 	}
-
-	// public function getCategories()   
-	// {
-	// 	$categoryModel = Ccc::getModel('Category');
-	// 	$category = $categoryModel->fetchAll("SELECT name, path FROM categories ORDER BY path");
-	// 	return $category;
-	// }
-
-	// public function getCategory()   
-	// {
-	// 	return $this->getData('categoryRow');
-	// }
-
-	// public function getParent() 
-	// {
-	// 	$id = $this->getCategory()->categoryId;
-	// 	$parentList = Ccc::getModel('Category')->fetchAll("SELECT path FROM categories WHERE path NOT LIKE '%$id%'");
-	// 	return $parentList;
-	// }
-
-	// public function path($path)
-	// {	
-	// 	$category = Ccc::getModel('Category');
-	// 	$pathArray = explode("/", $path);
-	// 	$temp1 = [];
-	// 		foreach ($pathArray as $value) {
-	// 			$temp2 = $category->fetchRow("SELECT name FROM categories WHERE categoryId = '$value'");
-	// 			$temp1[] = $temp2->name;
-
-	// 		}
-			
-	// 	$finalPath = implode("=>", $temp1); 	
-	// 	return $finalPath;	
-				
-	// }
 }
